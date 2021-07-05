4eec38252255b24a2c658483fc5e179ff741a052
unknown <amanayankhan78@gmail.com>
2021-07-05 12:01:55 +0530

    allowed host added

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gpoakz(*ohnd221x^cs_&!%=1yzyy^538=o=@r+3x92jv)-o$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','angelmix.herokuapp.com']
 
 
 # Application definition
